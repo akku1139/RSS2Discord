16e6a823021610aef51d1b617815ebd95143979e
-akku- <151517253+akku1139@users.noreply.github.com>
2024-05-10 15:29:59 +0900

    a

--- feeds.ts
+++ feeds.ts
@@ -1,14 +1,24 @@
-const feeds: Array<{
+const rawFeeds: Array<{
   name: string,
   url: string,
   icon?: string,
+  type?: "rss1" | "rss2" | "atom" | string,
   host?: string, // Proxyしたフィードのホスト名
+  base?: string, // WebHook設定用
 }> = [
   {
     name: "虚構新聞",
     url: "https://kyoko-np.net/index.xml",
     icon: "https://kyoko-np.net/images/app.png",
   },
-]
+];
 
-export default feeds;
+const webhooks = JSON.parse(Deno.env.get("WEBHOOKS"));
+
+export default rawFeeds.map((feed) => ({
+  name: feed.name,
+  url: feed.url,
+  icon: feed.icon ?? undefined,
+  host: feed.host ?? new URL(host.url).host,
+  webhook: webhooks[feed.base ?? feed.url],
+}));;
