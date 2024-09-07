33c522acf1f3bb1ebd10e7b1144cf5173bb06eaf
-akku- <151517253+akku1139@users.noreply.github.com>
2024-09-07 15:56:39 +0000

    多分ほぼ役に立たないであろうデバッグコード

--- main.ts
+++ main.ts
@@ -8,60 +8,63 @@ const db = await Deno.openKv("./data.db")
 const invalidFeeds: Array<string> = []
 
 feeds.forEach(
-  (feed) => fetch(feed.url).then(async (res) => {
-    if(!res.ok) {
-      invalidFeeds.push(feed.host ?? feed.url);
-    }
+  (feed) => {
+    console.log(feed.name)
+    fetch(feed.url).then(async (res) => {
+      if(!res.ok) {
+        invalidFeeds.push(feed.host ?? feed.url);
+      }
 
-    if(typeof feed.system === "undefined") {
-      const data = await parseFeed(await res.text())
-      data.entries.forEach(async (e) => {
-        const url = e.links[0].href
-        if(typeof url === "undefined") {
-          throw new Error(`in feed ${feed.name} (${feed.url}): url is undefined`)
-        }
-
-        if((await db.get([url])).value !== null) {
-          return
-        }
-
-        let retryCount = 0
-        while(true) {
-          const r = await fetch(feed.webhook, {
-            method: "POST",
-            headers: { 'Content-type': "application/json" },
-            body: JSON.stringify({
-              username: feed.name,
-              avatar_url: feed.icon,
-              embeds: [{
-                url,
-                color: 16777215,
-                title: e?.title?.value?.substr(0, 256).padStart(1, "-"),
-                description: e?.description?.value?.substr(0, 4096),
-                timestamp: e?.published,
-                thumbnail: (e?.attachments ?? [])[0]?.url
-              }]
-            })
-          })
-
-          if(r.ok) {
-            db.set([url], "a")
-            break
-          } else if(retryCount > 5) {
-            break
-          } else if(r.status === 429) {
-            await sleep((await r.json()).retry_after)
-            retryCount ++
-          } else {
-            console.error(r)
-            break
+      if(typeof feed.system === "undefined") {
+        const data = await parseFeed(await res.text())
+        data.entries.forEach(async (e) => {
+          const url = e.links[0].href
+          if(typeof url === "undefined") {
+            throw new Error(`in feed ${feed.name} (${feed.url}): url is undefined`)
           }
-        }
-      })
-    } // else {
-      // const data = await feed.system.parser(
-      //   await feed.system.loader(res)
-      // )
-    // }
-  }) // .catch();
+
+          if((await db.get([url])).value !== null) {
+            return
+          }
+
+          let retryCount = 0
+          while(true) {
+            const r = await fetch(feed.webhook, {
+              method: "POST",
+              headers: { 'Content-type': "application/json" },
+              body: JSON.stringify({
+                username: feed.name,
+                avatar_url: feed.icon,
+                embeds: [{
+                  url,
+                  color: 16777215,
+                  title: e?.title?.value?.substr(0, 256).padStart(1, "-"),
+                  description: e?.description?.value?.substr(0, 4096),
+                  timestamp: e?.published,
+                  thumbnail: (e?.attachments ?? [])[0]?.url
+                }]
+              })
+            })
+
+            if(r.ok) {
+              db.set([url], "a")
+              break
+            } else if(retryCount > 5) {
+              break
+            } else if(r.status === 429) {
+              await sleep((await r.json()).retry_after)
+              retryCount ++
+            } else {
+              console.error(r)
+              break
+            }
+          }
+        })
+      } // else {
+        // const data = await feed.system.parser(
+        //   await feed.system.loader(res)
+        // )
+      // }
+    }) // .catch();
+  }
 )
