98fda2a507fa1e4c5f21407324d9db54c0724a28
-akku- <151517253+akku1139@users.noreply.github.com>
2024-09-07 22:55:45 +0900

    b

--- feeds.ts
+++ feeds.ts
@@ -1,5 +1,3 @@
-import { parseFeed } from "rss";
-
 interface RawFeed {
   name: string,
   url: string,
@@ -14,22 +12,27 @@ const rawFeeds: Array<RawFeed> = [
     url: "https://kyoko-np.net/index.xml",
     icon: "https://kyoko-np.net/images/app.png",
   },
-];
+]
 
-const webhooks = JSON.parse(Deno.env.get("WEBHOOK_URL")) as {
+const webhooks = JSON.parse(
+  await (
+    await fetch(
+      Deno.env.get("WEBHOOK_URL")
+    )
+  ).json()) as {
   [key: string]: string
-};
+}
 
 interface FormattedFeed extends RawFeed {
   webhook: string,
-  loader: (res: Response) => Promise<string>,
-  parser: (data: string) => Promise<object>,
+  system?: {
+    loader: (res: Response) => Promise<string>,
+    parser: (data: string) => Promise<object>,
+  }
 }
 
 export default rawFeeds.map((feed): FormattedFeed => ({
   ...feed,
   host: feed.host ?? new URL(feed.url).host,
   webhook: webhooks[feed.base ?? feed.url] ?? webhooks.default,
-  loader: (r) => r.text(),
-  parser: parseFeed,
-}));
+}))
--- main.ts
+++ main.ts
@@ -1,19 +1,30 @@
+import { parseFeed } from "rss"
 
-import feeds from "./feeds.ts";
+import feeds from "./feeds.ts"
 
+const db = JSON.parse(Deno.readTextFileSync("./db.json")) as {[key: string]: "a"}
 
-let invalidFeeds: Array<string> = [];
+let invalidFeeds: Array<string> = []
 
-let rateLimited = 0;
+let rateLimited = 0
 
 feeds.forEach(
   (feed) => fetch(feed.url).then(async (res) => {
     if(!res.ok) {
       invalidFeeds.push(feed.host ?? feed.url);
     }
-    const data = await feed.parser(await feed.loader(res));
-    data.entries.forEach((entry) => {
-      console.log(entry);
-    });
+
+    if(typeof feed.system === "undefined") {
+      const data = await parseFeed(await res.text())
+      data.entries.forEach((entry) => {
+        console.log(entry)
+      })
+    } else {
+      const data = await feed.system.parser(
+        await feed.system.loader(res)
+      )
+    }
   }) // .catch();
-);
+)
+
+Deno.writeTextFileSync("./data.json", JSON.stringify(db))
