3983b2319b24a5d56bd493adfda3ce3d0f7424d8
-akku- <151517253+akku1139@users.noreply.github.com>
2024-09-08 00:02:08 +0900

    g

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 import { parseFeed } from "rss"
 
 import feeds from "./feeds.ts"
+import { sleep } from "./utils.js"
 
 const db = JSON.parse(Deno.readTextFileSync("./db.json")) as {[key: string]: "a"}
 
 let invalidFeeds: Array<string> = []
-
-let rateLimited = 0
 
 feeds.forEach(
   (feed) => fetch(feed.url).then(async (res) => {
@@ -18,22 +17,42 @@ feeds.forEach(
       const data = await parseFeed(await res.text())
       data.entries.forEach(async (e) => {
         const url = e.links[0].href
-        await fetch(feed.webhook, {
-          method: "POST",
-          headers: { 'Content-type': "application/json" },
-          body: JSON.stringify({
-            username: feed.name,
-            avatar_url: feed.icon,
-            embeds: [{
-              url,
-              color: 16777215,
-              title: e?.title.value.substr(0, 256).padStart(1, "-"),
-              description: e?.description.value.substr(0, 4096),
-              timestamp: e?.published,
-              thumbnail: (e?.attachments ?? [])[0]?.url
-            }]
+
+        if(Object.hasOwn(db, url)) {
+          return
+        }
+
+        let retryCount = 0
+        while(true) {
+          const r = await fetch(feed.webhook, {
+            method: "POST",
+            headers: { 'Content-type': "application/json" },
+            body: JSON.stringify({
+              username: feed.name,
+              avatar_url: feed.icon,
+              embeds: [{
+                url,
+                color: 16777215,
+                title: e?.title.value.substr(0, 256).padStart(1, "-"),
+                description: e?.description.value.substr(0, 4096),
+                timestamp: e?.published,
+                thumbnail: (e?.attachments ?? [])[0]?.url
+              }]
+            })
           })
-        })
+          if(r.ok) {
+            db[url] = "a"
+            break
+          } else if(retryCount > 5) {
+            break
+          } else if(r.status === 429) {
+            await sleep((await r.json()).retry_after)
+            retryCount ++
+          } else {
+            console.error(r)
+            break
+          }
+        }
       })
     } // else {
       // const data = await feed.system.parser(
@@ -42,5 +61,9 @@ feeds.forEach(
     // }
   }) // .catch();
 )
+
+
+
+console.log("failed feeds", invalidFeeds)
 
 Deno.writeTextFileSync("./data.json", JSON.stringify(db))
--- utils.ts
+++ utils.ts
@@ -1,6 +1,6 @@
 export const addSIPrefix = (e) => {
   const units = ["", "K", "M", "G", "T", "P", "E", "Z", "Y"];
-  let o = parseFloat(e, 10);
+  let o = parseFloat(e);
   let n = 0
   for (; o >= 1e3;) {
     o /= 1000;
@@ -13,3 +13,5 @@ export const timeSince = (e) => {
   let t = Math.floor(e / 31536e3);
   return t > 1 ? t + " y" : (t = Math.floor(e / 2592e3)) > 1 ? t + " months" : (t = Math.floor(e / 86400)) > 1 ? t + " days" : (t = Math.floor(e / 3600)) > 1 ? t + " hours" : (t = Math.floor(e / 60)) > 1 ? t + " min" : "NOW"
 }
+
+export const sleep = s => new Promise(resolve => setTimeout(resolve, 1000 * s))
