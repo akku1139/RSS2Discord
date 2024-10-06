9f958d4c26b56dee93e3647beade11cc01667dba
-akku- <151517253+akku1139@users.noreply.github.com>
2024-10-07 01:15:48 +0900

    削除したプラグインを削除

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,8 +1,7 @@
 import { cleanAllURLParams } from "./plugins/urlClean.ts"
-import { unEscapeHTML, enhancedUnEscapeHTML } from "./plugins/htmlUnEscape.ts"
+import { unEscapeHTML } from "./plugins/htmlUnEscape.ts"
 
 export const plugins = {
   cleanAllURLParams,
   unEscapeHTML,
-  enhancedUnEscapeHTML,
 }
