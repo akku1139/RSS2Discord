05e6bf6776a7e78b4f14077efd86747bfa463b54
-akku- <151517253+akku1139@users.noreply.github.com>
2024-10-21 21:57:56 +0900

    移動

--- src/feeds/miningpoolstats.ts
+++ src/feeds/miningpoolstats.ts
@@ -1,4 +1,20 @@
-import { addSIPrefix, timeSince, makeFeeds } from "../utils.ts"
+import { makeFeeds } from "../utils.ts"
+
+const addSIPrefix = (e: string) => {
+  const units = ["", "K", "M", "G", "T", "P", "E", "Z", "Y"];
+  let o = parseFloat(e);
+  let n = 0
+  for (; o >= 1e3;) {
+    o /= 1000;
+    n++;
+  }
+  return o.toFixed(2) + " " + units[n]
+}
+
+const timeSince = (e: number) => {
+  let t = Math.floor(e / 31536e3);
+  return t > 1 ? t + " y" : (t = Math.floor(e / 2592e3)) > 1 ? t + " months" : (t = Math.floor(e / 86400)) > 1 ? t + " days" : (t = Math.floor(e / 3600)) > 1 ? t + " hours" : (t = Math.floor(e / 60)) > 1 ? t + " min" : "NOW"
+}
 
 export default makeFeeds({
   name: "Mining Pool Stats新コイン通知",
--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,5 @@
 import type { Plugin, RawFeed } from "./types.ts"
 
-export const addSIPrefix = (e: string) => {
-  const units = ["", "K", "M", "G", "T", "P", "E", "Z", "Y"];
-  let o = parseFloat(e);
-  let n = 0
-  for (; o >= 1e3;) {
-    o /= 1000;
-    n++;
-  }
-  return o.toFixed(2) + " " + units[n]
-}
-
-export const timeSince = (e: number) => {
-  let t = Math.floor(e / 31536e3);
-  return t > 1 ? t + " y" : (t = Math.floor(e / 2592e3)) > 1 ? t + " months" : (t = Math.floor(e / 86400)) > 1 ? t + " days" : (t = Math.floor(e / 3600)) > 1 ? t + " hours" : (t = Math.floor(e / 60)) > 1 ? t + " min" : "NOW"
-}
-
 export const sleep = (s: number) => new Promise(resolve => setTimeout(resolve, 1000 * s))
 
 export const filterUndefined = (a: Array<unknown>) => a.filter(e => {
