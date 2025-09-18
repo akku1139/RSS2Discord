2125ce775c35b7df850df685cb1b1c1d1c4b58fe
-akku- <151517253+akku1139@users.noreply.github.com>
2025-09-18 18:41:42 +0900

    多分直す

--- src/plugins/linuxReleaseID.ts
+++ src/plugins/linuxReleaseID.ts
@@ -1,9 +1,23 @@
 import { makePlugin } from "../utils.ts"
 
+// simple hash by gemini 2.5 flash
+function simpleHash(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    // 32ビット整数に収めるためのビット演算
+    hash |= 0;
+  }
+  // 結果を36進数に変換し、文字列として返す
+  // 10文字程度に短縮したい場合は、ここで substring を使う
+  return (hash >>> 0).toString(36).substring(0, 10);
+}
+
 export const linuxReleaseID = makePlugin({
   transformer: {
     url: (link, e) => {
-      return link + "#" + e?.title?.value
+      return link + "#" + simpleHash(e?.title?.value)
     }
   }
 })
