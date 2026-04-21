58af9f84f94d4e8c125588384891b4e11c1f5bd3
Akari Tsuyukusa <akkun11.open@gmail.com>
2026-04-21 19:28:11 +0900

    fix gazlog icon

--- src/feed.ts
+++ src/feed.ts
@@ -354,7 +354,7 @@ const rawFeeds = makeFeeds(
   }, {
     name: "GAZLOG",
     url: "https://gazlog.jp/feed/",
-    icon: "https://gazlog.com/wp-content/uploads/2022/12/cropped-Gazlog-favcon-3-192x192.jpg",
+    icon: "https://gazlog.jp/wp-content/uploads/2022/12/cropped-Gazlog-favcon.jpg",
     plugins: ["unHTML"],
   }, {
     name: "アナログ",
